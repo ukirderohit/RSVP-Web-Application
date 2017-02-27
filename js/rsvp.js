5181ff6b56d11c10fcd78cfced2cbbee6c286497
Rohit Ukirde <ukirderohit@gmail.com>
2017-02-26 21:31:16 -0600

    Js will add new elements <li> to <ul> for given input. also added the validation for input values the message will be displayed on the input placeholder

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,1 +1,19 @@
 console.log("Hello from Console");
+const formIdRegistrar = document.getElementById('registrar');
+const formInput = formIdRegistrar.getElementsByTagName('input')[0];
+const formButton = formIdRegistrar.getElementsByTagName('button')[0];
+
+formIdRegistrar.addEventListener('submit',(e)=>{
+    e.preventDefault(); //to cancel the event to show o/p
+    
+    let ul = document.getElementById('invitedList');
+    let li = document.createElement('li');
+        li.textContent = formInput.value;
+        if (li.textContent == '') {
+            formInput.placeholder = "Please Enter the Correct Name"
+        } else {
+            ul.appendChild(li);
+            formInput.placeholder = "Invite Someone"
+        }
+        formInput.value = '';
+});
